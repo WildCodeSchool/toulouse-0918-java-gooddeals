860e472bcb7a7b86aea2b49802726ac1dcbd0aaf
Richard Fages <richard.fages@gmail.com>
2018-10-05 11:45:59 +0200

    convert Listactivity in Listfragment and fixed several bugs

--- app/src/main/java/fr/wildcodeschool/gooddeals/ListFragment.java
+++ app/src/main/java/fr/wildcodeschool/gooddeals/ListFragment.java
@@ -1,29 +1,39 @@
 package fr.wildcodeschool.gooddeals;
 
+import android.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import static fr.wildcodeschool.gooddeals.MapFragment.dealArrayList;
 
-public class ListFragment extends AppCompatActivity {
+public class ListFragment extends android.support.v4.app.Fragment {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_deals);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_deals, container, false);
+    }
 
-        ListView listDeals = findViewById(R.id.list_view_deals);
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ListView listDeals = getView().findViewById(R.id.list_view_deals);
         DealsAdapter adapter = new DealsAdapter(listDeals.getContext(), dealArrayList());
         listDeals.setAdapter(adapter);
 
         listDeals.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(ListFragment.this, "Clic sur la liste", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), R.string.clicliste, Toast.LENGTH_SHORT).show();
             }
         });
     }
