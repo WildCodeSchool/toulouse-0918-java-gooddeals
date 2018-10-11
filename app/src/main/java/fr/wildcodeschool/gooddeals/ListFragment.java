b88826a62b261803ee5f019c4e14933b9340ba7e
Jérémy Fruit <jeremy.fruit@gmail.com>
2018-10-11 12:02:01 +0200

    delete arraylist et firebase

--- app/src/main/java/fr/wildcodeschool/gooddeals/ListFragment.java
+++ app/src/main/java/fr/wildcodeschool/gooddeals/ListFragment.java
@@ -13,7 +13,17 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import static fr.wildcodeschool.gooddeals.MapFragment.dealArrayList;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 
 public class ListFragment extends android.support.v4.app.Fragment {
 
@@ -26,15 +36,38 @@ public class ListFragment extends android.support.v4.app.Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
+        final ArrayList<Deal> deals = new ArrayList<>();
         final ListView listDeals = getView().findViewById(R.id.list_view_deals);
-        DealsAdapter adapter = new DealsAdapter(listDeals.getContext(), dealArrayList());
+        final DealsAdapter adapter = new DealsAdapter(listDeals.getContext(), deals);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        DatabaseReference dealRef = database.getReference("deal");
+        dealRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //dealArrayList().clear();
+                for (DataSnapshot dealSnapshot : dataSnapshot.getChildren()) {
+                    Deal deal = dealSnapshot.getValue(Deal.class);
+                    deals.add(deal);
+
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
         listDeals.setAdapter(adapter);
 
         listDeals.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Deal deal = dealArrayList().get(i);
+                Deal deal = deals.get(i);
                 Intent intent = new Intent(getActivity(), Popup.class);
                 intent.putExtra("EXTRA_DESCRIPTION", deal.getDescription());
                 intent.putExtra("EXTRA_TITLE", deal.getName());
